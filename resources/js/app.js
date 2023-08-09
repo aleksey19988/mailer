a31ec68c7444e6b12bc91758ff432acbf7563bb4
Aleksey Kononenko <aleksey19988@gmail.com>
2023-08-09 10:36:54 +0300

    - Рефакторинг

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,33 +29,33 @@ $("#api-request-form").on("submit", function(event){
         }
     });
 });
-// $("#send-email-form").on("submit", function(event){
-//     event.preventDefault();
-//
-//     $.ajax({
-//         url: $(this).prop('action'),
-//         method: 'post',
-//         dataType: 'html',
-//         data: $(this).serialize(),
-//         beforeSend: function () {
-//             $('.spinner-container').css('display', 'flex');
-//         },
-//         complete: function () {
-//             $('.spinner-container').css('display', 'none');
-//         },
-//         error: function (data) {
-//             $('.spinner-container').css('display', 'none');
-//             console.log(data);
-//             let jsonData = JSON.parse(data);
-//
-//             $('.congratulation-error-content').html(`Ошибочка: ${jsonData.result}`);
-//             $('.congratulation-error-alert').show();
-//         },
-//         success: function(data){
-//             let jsonData = JSON.parse(data);
-//
-//             $('.congratulation-success-content').html(`<p><strong>Почта получателя:</strong> ${jsonData.email}</p><p><strong>Поздравление:</strong> ${jsonData.congratulationMessage}</p>`);
-//             $('.congratulation-success-alert').show();
-//         }
-//     });
-// });
+$("#send-email-form").on("submit", function(event){
+    event.preventDefault();
+
+    $.ajax({
+        url: $(this).prop('action'),
+        method: 'post',
+        dataType: 'html',
+        data: $(this).serialize(),
+        beforeSend: function () {
+            $('.spinner-container').css('display', 'flex');
+        },
+        complete: function () {
+            $('.spinner-container').css('display', 'none');
+        },
+        error: function (data) {
+            $('.spinner-container').css('display', 'none');
+            console.log(data);
+            let jsonData = JSON.parse(data);
+
+            $('.congratulation-error-content').html(`Ошибочка: ${jsonData.result}`);
+            $('.congratulation-error-alert').show();
+        },
+        success: function(data){
+            let jsonData = JSON.parse(data);
+
+            $('.congratulation-success-content').html(`<p><strong>Почта получателя:</strong> ${jsonData.email}</p><p><strong>Поздравление:</strong> ${jsonData.congratulationMessage}</p>`);
+            $('.congratulation-success-alert').show();
+        }
+    });
+});
