b8f93912fa94e6c2b5c628faa1e4196656eb7266
Aleksey Kononenko <aleksey19988@gmail.com>
2023-08-14 13:56:37 +0300

    fix: Редактирвоание миграции

--- database/migrations/2023_08_14_131919_update_letter_body_column_in_email_log_table.php
+++ database/migrations/2023_08_14_131919_update_letter_body_column_in_email_log_table.php
@@ -11,32 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Добавляю новое поле с большей длиной
-        Schema::table('email_log', function (Blueprint $table) {
-            $table->string('letter_body_new', 5000)->after('letter_subject')->comment('Тело письма');
-        });
-
-        // Копирую все данные из старого поля в новое
-        $emailLogs = \App\Models\EmailLog::all();
-        if ($emailLogs) {
-            foreach($emailLogs as $log) {
-                $log->letter_body_new = $log->letter_body;
-                $log->save();
-            }
-        }
-
-        // Удаляю старое поле
         Schema::table('email_log', function(Blueprint $table)
         {
-            // 3- delete the old column
             $table->dropColumn('letter_body');
         });
 
-        // Меняю имя нового поля на старое
         Schema::table('email_log', function(Blueprint $table)
         {
-            // 4- rename the new column to match name of the deleted column
-            $table->renameColumn('letter_body_new', 'letter_body');
+            $table->string('letter_body', 5000)->after('letter_subject')->comment('Тело письма');
         });
     }
 
